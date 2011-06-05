fa4753b336bde44d096f0285eb95fcc4fed1f9a5
Iain Nash <info@codejoust.com>
2011-06-04 22:54:01 -0400

    Moving to coffee for clientside js and cleaning up js files

---
+++ public/scripts/redisviewer.coffee
@@ -0,0 +1,114 @@
+class PageView
+  @active = []
+  @are_keys: true
+  
+  constructor: ->
+    @results = $('ul#results')
+    $('a.confirm').live('click', @link_click)
+    @command_el = $('#command')
+    @command_el.bind('keyup', @cmd_keyup)
+    $(document).bind('keydown', @doc_key)
+  
+  getTpl: (id) ->
+    document.getElementById(id).innerHTML
+  
+  load_templates: () ->
+    @templates =
+      keys:    _.template(@getTpl('key-template'))
+      bulk:    _.template(@getTpl('bulk-template'))
+      status:  _.template(@getTpl('status-template'))
+      error:   _.template(@getTpl('error-template'))
+      hash:    _.template(@getTpl('hash-template'))
+      list:    _.template(@getTpl('list-template'))
+      set:     _.template(@getTpl('set-template'))
+      zset:    _.template(@getTpl('zset-template'))
+      integer: _.template(@getTpl('integer-template'))
+  
+  cmd_keyup: (evt) ->
+    if evt.keyCode == 13
+      $('#command').val('')
+  
+  goto: (hash) ->
+    window.location.hash = hash
+    
+  show_active: ->
+    if (@active.index() % 2 == 0)
+      topheight = (@active.offset().top - (1/3)*$(window).height())
+      $('body').stop().animate({scrollTop: topheight}, 600)
+    
+  doc_key: (evt) =>
+    unless (@command_el.is(':focus'))
+      switch evt.keyCode
+        when 191 then @command_el.focus()
+        when 73 then @goto('#INFO')
+        when 75, 81 then @goto('#KEYS *')
+        when 74, 38
+          if @active.prev().length > 0
+            @active = @active.removeClass('active').prev().addClass('active')
+            @show_active()
+            evt.preventDefault()
+        when 75, 40
+          if @active.next().length > 0
+            @active = @active.removeClass('active').next().addClass('active')
+            @show_active()
+            evt.preventDefault()
+        when 79     then @goto(@active.find('a').attr('href'))
+        when 13, 39 then @goto(@active.find('a').attr('href'))
+        when 88
+          if @active.find('a.confirm').length && @link_click()
+            @goto(@active.find('a.confirm').attr('href'))
+        when 37 then socket.goback()
+        
+ 
+  link_click: () ->
+    confirm('Are you sure you want to run this action?')
+  
+  clear_results: ->
+    @results.empty()
+  
+  update_content: (type, message) ->
+    @load_templates() unless @templates?
+    @results.append @templates[type](message)
+    if @results.has('.active').length == 0
+      @active = @results.find('li:first-child').addClass('active')
+  
+class SocketHandler
+  constructor: ->
+    @curhash = window.location.hash
+    @socket = new io.Socket(location.hostname)
+    @socket.connect()
+    $(window).bind('hashchange', @on_hashchange)
+    @socket.on('message', @handle_message)
+    
+  update: (id, html) ->
+    document.getElementById(id).innerHTML = html
+  
+  handle_message: (message) =>
+    @update 'title', message.title
+    @update 'subtitle', message.reply_type
+    page.are_keys = !!(message.reply_type == 'keys')
+    page.update_content(message.reply_type, message)
+  
+  on_hashchange: =>
+    @oldhash = @curhash
+    @curhash = window.location.hash
+    @send_command(window.location.hash.substr(1)) if window.location.hash.length > 1
+  
+  send_command: (command) =>
+    page.clear_results()
+    if (!command)
+      command = 'KEYS *'
+    return @socket.send(command)
+    
+  loaded: ->
+    @send_command()
+    
+  goback: ->
+    window.location.hash = @oldhash
+    @curhash = @oldhash
+  
+  
+$ ->
+  window.page = page =     new PageView()
+  window.socket = socket = new SocketHandler()
+  socket.loaded()
---
+++ public/scripts/redisviewer.js
@@ -0,0 +1,153 @@
+(function() {
+  var PageView, SocketHandler;
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  PageView = (function() {
+    PageView.active = [];
+    PageView.are_keys = true;
+    function PageView() {
+      this.doc_key = __bind(this.doc_key, this);;      this.results = $('ul#results');
+      $('a.confirm').live('click', this.link_click);
+      this.command_el = $('#command');
+      this.command_el.bind('keyup', this.cmd_keyup);
+      $(document).bind('keydown', this.doc_key);
+    }
+    PageView.prototype.getTpl = function(id) {
+      return document.getElementById(id).innerHTML;
+    };
+    PageView.prototype.load_templates = function() {
+      return this.templates = {
+        keys: _.template(this.getTpl('key-template')),
+        bulk: _.template(this.getTpl('bulk-template')),
+        status: _.template(this.getTpl('status-template')),
+        error: _.template(this.getTpl('error-template')),
+        hash: _.template(this.getTpl('hash-template')),
+        list: _.template(this.getTpl('list-template')),
+        set: _.template(this.getTpl('set-template')),
+        zset: _.template(this.getTpl('zset-template')),
+        integer: _.template(this.getTpl('integer-template'))
+      };
+    };
+    PageView.prototype.cmd_keyup = function(evt) {
+      if (evt.keyCode === 13) {
+        return $('#command').val('');
+      }
+    };
+    PageView.prototype.goto = function(hash) {
+      return window.location.hash = hash;
+    };
+    PageView.prototype.show_active = function() {
+      var topheight;
+      if (this.active.index() % 2 === 0) {
+        topheight = this.active.offset().top - (1 / 3) * $(window).height();
+        return $('body').stop().animate({
+          scrollTop: topheight
+        }, 600);
+      }
+    };
+    PageView.prototype.doc_key = function(evt) {
+      if (!(this.command_el.is(':focus'))) {
+        switch (evt.keyCode) {
+          case 191:
+            return this.command_el.focus();
+          case 73:
+            return this.goto('#INFO');
+          case 75:
+          case 81:
+            return this.goto('#KEYS *');
+          case 74:
+          case 38:
+            if (this.active.prev().length > 0) {
+              this.active = this.active.removeClass('active').prev().addClass('active');
+              this.show_active();
+              return evt.preventDefault();
+            }
+            break;
+          case 75:
+          case 40:
+            if (this.active.next().length > 0) {
+              this.active = this.active.removeClass('active').next().addClass('active');
+              this.show_active();
+              return evt.preventDefault();
+            }
+            break;
+          case 79:
+            return this.goto(this.active.find('a').attr('href'));
+          case 13:
+          case 39:
+            return this.goto(this.active.find('a').attr('href'));
+          case 88:
+            if (this.active.find('a.confirm').length && this.link_click()) {
+              return this.goto(this.active.find('a.confirm').attr('href'));
+            }
+            break;
+          case 37:
+            return socket.goback();
+        }
+      }
+    };
+    PageView.prototype.link_click = function() {
+      return confirm('Are you sure you want to run this action?');
+    };
+    PageView.prototype.clear_results = function() {
+      return this.results.empty();
+    };
+    PageView.prototype.update_content = function(type, message) {
+      if (this.templates == null) {
+        this.load_templates();
+      }
+      this.results.append(this.templates[type](message));
+      if (this.results.has('.active').length === 0) {
+        return this.active = this.results.find('li:first-child').addClass('active');
+      }
+    };
+    return PageView;
+  })();
+  SocketHandler = (function() {
+    function SocketHandler() {
+      this.send_command = __bind(this.send_command, this);;
+      this.on_hashchange = __bind(this.on_hashchange, this);;
+      this.handle_message = __bind(this.handle_message, this);;      this.curhash = window.location.hash;
+      this.socket = new io.Socket(location.hostname);
+      this.socket.connect();
+      $(window).bind('hashchange', this.on_hashchange);
+      this.socket.on('message', this.handle_message);
+    }
+    SocketHandler.prototype.update = function(id, html) {
+      return document.getElementById(id).innerHTML = html;
+    };
+    SocketHandler.prototype.handle_message = function(message) {
+      this.update('title', message.title);
+      this.update('subtitle', message.reply_type);
+      page.are_keys = !!(message.reply_type === 'keys');
+      return page.update_content(message.reply_type, message);
+    };
+    SocketHandler.prototype.on_hashchange = function() {
+      this.oldhash = this.curhash;
+      this.curhash = window.location.hash;
+      if (window.location.hash.length > 1) {
+        return this.send_command(window.location.hash.substr(1));
+      }
+    };
+    SocketHandler.prototype.send_command = function(command) {
+      page.clear_results();
+      if (!command) {
+        command = 'KEYS *';
+      }
+      return this.socket.send(command);
+    };
+    SocketHandler.prototype.loaded = function() {
+      return this.send_command();
+    };
+    SocketHandler.prototype.goback = function() {
+      window.location.hash = this.oldhash;
+      return this.curhash = this.oldhash;
+    };
+    return SocketHandler;
+  })();
+  $(function() {
+    var page, socket;
+    window.page = page = new PageView();
+    window.socket = socket = new SocketHandler();
+    return socket.loaded();
+  });
+}).call(this);
